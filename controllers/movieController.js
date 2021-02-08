06c2b81d1ec739d5f52bcdbd138662682c45d8cd
dansantander <dansantandr@gmail.com>
2021-02-08 16:24:37 -0600

    feat: add movie form

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,7 @@
 var Movie = require('../models/movie');
 var Genre = require('../models/genre');
-
 var async = require('async');
+const { body, validationResult } = require("express-validator");
 
 exports.index = function(req, res) {
   async.parallel({
@@ -45,14 +45,77 @@ exports.movie_detail = function(req, res, next) {
 };
 
 // Display Movie create form on GET.
-exports.movie_create_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Movie create GET');
+exports.movie_create_get = function(req, res, next) {
+  Genre.find({}, 'name')
+  .exec(function(err, genres) {
+    if (err) { return next(err); }
+    res.render('movie_form', { title: 'Create Movie', genres: genres });
+  })
 };
 
 // Handle Movie create on POST.
-exports.movie_create_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Movie create POST');
-};
+exports.movie_create_post = [
+
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if(!(req.body.genre instanceof Array)){
+        if(typeof req.body.genre ==='undefined')
+        req.body.genre = [];
+        else
+        req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  // Validate and sanitise fields.
+  body('title').isLength({ min: 1}).escape(),
+  body('description').isLength({ min: 1}).escape().withMessage('Description must be specified.'),
+  body('price').isLength({ min: 1}).escape(),
+  body('inStock').isLength({ min: 1}).escape(),
+  body('genre.*').escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    var movie = new Movie(
+    { title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      inStock: req.body.inStock,
+      genre: req.body.genre
+     });
+
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      Genre.find({}, 'name')
+      .exec(function(err, genres) {
+        if (err) { return next(err); }
+
+        // Mark our selected genres as checked.
+        for (let i = 0; i < results.genres.length; i++) {
+          if (book.genre.indexOf(results.genres[i]._id) > -1) {
+              results.genres[i].checked='true';
+          }
+        }
+
+        res.render('movie_form', { title: 'Create Movie', genres: genres, errors: errors.array() });
+
+      })
+      return;
+    } else {
+
+      // Data from form is valid. Save book.
+      movie.save(function (err) {
+        if (err) { return next(err); }
+           //successful - redirect to new book record.
+           res.redirect(movie.url);
+        });
+    }
+
+  }
+]
 
 // Display Movie delete form on GET.
 exports.movie_delete_get = function(req, res) {
